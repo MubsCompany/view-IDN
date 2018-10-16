0344029d0732f71ab1b4b0dab7766f337b726100
Muhammad Basalamah <muhammad@Testtests-MacBook-Pro.local>
2018-10-16 13:57:28 +0700

    Initial Commit

---
+++ view IDN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  view IDN
+//
+//  Created by Muhammad Basalamah on 16/10/18.
+//  Copyright © 2018 Mubs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ view IDNTests/view_IDNTests.swift
@@ -0,0 +1,36 @@
+//
+//  view_IDNTests.swift
+//  view IDNTests
+//
+//  Created by Muhammad Basalamah on 16/10/18.
+//  Copyright © 2018 Mubs. All rights reserved.
+//
+
+import XCTest
+@testable import view_IDN
+
+class view_IDNTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
